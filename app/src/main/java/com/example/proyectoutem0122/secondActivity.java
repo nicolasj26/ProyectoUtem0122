017007b23b057c9d2b449deb2f8a645c32839d0e
nicolasj26 <nicolas.jimenezj@utem.cl>
2022-05-09 04:34:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/proyectoutem0122/secondActivity.java
@@ -0,0 +1,20 @@
+package com.example.proyectoutem0122;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class secondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void IrAtras(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
